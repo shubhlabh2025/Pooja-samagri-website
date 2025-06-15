e5b8c6433daf9ecb048d323c00302ee7d0e96fe6
mahiranand <mahiranand08@gmail.com>
2025-06-15 11:59:41 +0530

    crausal fixed

--- src/components/ui/banner-carousel.tsx
+++ src/components/ui/banner-carousel.tsx
@@ -1,53 +1,80 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Card, CardContent } from "./card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  type CarouselApi,
+} from "./carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { cn } from "@/lib/utils";
 
 const BannerCarousel: React.FC = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
   const images = [
     "https://phool.co/cdn/shop/files/kedarnath_Home_page_banner_Mob.webp?v=1724320539&width=1000",
     "https://phool.co/cdn/shop/files/Banner_Mobile_edbb9fc3-266a-4a39-a9f3-d2a289917a12.jpg?v=1719235460&width=1000",
   ];
 
-  const currentIndexRef = useRef(0);
-  const intervalRef = useRef<NodeJS.Timeout | null>(null);
-
   useEffect(() => {
-    intervalRef.current = setInterval(() => {
-      currentIndexRef.current = (currentIndexRef.current + 1) % images.length;
-      setCurrentIndex(currentIndexRef.current);
-    }, 3000); // 3 seconds
+    if (!api) {
+      return;
+    }
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
 
-    return () => {
-      if (intervalRef.current) clearInterval(intervalRef.current);
-    };
-  }, [images.length]);
-  console.log("render");
+  const plugin = useRef(Autoplay({ delay: 3000, stopOnInteraction: false }));
 
   return (
-    <div className="relative max-h-[300px] w-full overflow-hidden rounded-lg shadow-lg">
-      <div
-        className="flex max-h-[300px] transition-transform duration-700 ease-in-out"
-        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+    <div className="relative">
+      <Carousel
+        plugins={[plugin.current]}
+        opts={{ loop: true }}
+        className="w-full"
+        setApi={setApi}
       >
-        {images.map((src, idx) => (
-          <img
-            key={idx}
-            src={src}
-            alt={`Slide ${idx}`}
-            className="max-h-[300px] min-w-full"
-          />
-        ))}
-      </div>
-
-      <div className="absolute bottom-4 left-1/2 flex -translate-x-1/2 space-x-2">
-        {images.map((_, idx) => (
-          <div
-            key={idx}
-            className={`h-2 w-2 rounded-full ${
-              idx === currentIndex ? "bg-white" : "bg-gray-400"
-            }`}
-          ></div>
-        ))}
-      </div>
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square max-h-[300px] items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+      {count > 0 && (
+        <div className="absolute bottom-5 left-1/2 flex -translate-x-1/2 transform justify-center gap-2">
+          {Array.from({ length: count }).map((_, index) => (
+            <button
+              key={index}
+              className={cn(
+                "relative h-1 overflow-hidden rounded-full transition-all duration-300",
+                index === current - 1
+                  ? "w-12 bg-[#00000021]"
+                  : "w-3 bg-[#00000021]",
+              )}
+              onClick={() => api?.scrollTo(index)}
+              aria-label={`Go to slide ${index + 1}`}
+            >
+              {index === current - 1 && (
+                <span className="animate-indicator-progress absolute top-0 w-full left-0 h-full rounded-full bg-black" />
+              )}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
